8d966f79d3a79620f4310ce75d568aed3d15ba0e
YourGitHubUsername <ramuk13476@gmail.com>
2025-09-04 23:52:55 +0530

    vehicle-page-done

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 
 export default function LocationSearchPanel() {
   return (
-    <div>LocationSearchPanel</div>
+    <div className='ml-4 mr-4'>
+<div className='flex  gap-4 my-3 items-center justify-start'>
+    <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i class="ri-map-pin-fill"></i></h2>
+    <h4 className='font-medium'> 24B, Near Kapoor's cafe, Newton Coding School, NewDelhi</h4>
+</div>
+<div className='flex  gap-4 my-3 items-center justify-start'>
+    <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i class="ri-map-pin-fill"></i></h2>
+    <h4 className='font-medium'> 24B, Near Kapoor's cafe, Newton Coding School, NewDelhi</h4>
+</div>
+<div className='flex  gap-4 my-3 items-center justify-start'>
+    <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i class="ri-map-pin-fill"></i></h2>
+    <h4 className='font-medium'> 24B, Near Kapoor's cafe, Newton Coding School, NewDelhi</h4>
+</div>
+<div className='flex  gap-4 my-3 items-center justify-start'>
+    <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i class="ri-map-pin-fill"></i></h2>
+    <h4 className='font-medium'> 24B, Near Kapoor's cafe, Newton Coding School, NewDelhi</h4>
+</div>
+<div className='flex  gap-4 my-3 items-center justify-start'>
+    <h2 className='bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full'><i class="ri-map-pin-fill"></i></h2>
+    <h4 className='font-medium'> 24B, Near Kapoor's cafe, Newton Coding School, NewDelhi</h4>
+</div>
+
+
+    </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -36,7 +36,7 @@ if(panelOpen){
 },[panelOpen])
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
        <img className='w-20 absolute left-5 top-5 ' src="https://www.logo.wine/a/logo/Uber/Uber-Logo.wine.svg" alt="" />
        <div className='h-screen w-screen'>
         <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
@@ -71,12 +71,57 @@ if(panelOpen){
           }}
             className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-4' type="text" placeholder='Enter your destination' />
         </form>
+      
       </div>
-      <div ref={panelRef} className='h-[0%] bg-white  '>
-        <LocationSearchPanel />
+        <div ref={panelRef} className='h-[0%] bg-white  '>
+          <LocationSearchPanel />
+      
 
       </div>
+        
        </div>
+       <div className=' fixed w-full z-10 bottom-0 bg-white px-2 py-5'>
+        <h3 className='text-2xl font-semibold mb-5'> Choose a Vehicle</h3>
+        <div className='flex w-full border-2 mb-2 active:border-black rounded-xl items-center p-3  justify-between'>
+       
+          <img className='h-20 mr-4' src="https://www.svgrepo.com/show/408292/car-white.svg" alt="" />
+          <div className=' w-1/2'>
+            <h4 className='font-medium text-lg'>UberGo <span><i className="ri-user-fill"></i>4</span></h4>
+            <h5 className='font-medium text-md'>2 mins away</h5>
+            <p className='font-medium text-sm text-gray-600'>Affordable, compact rides</p>
+          </div>
+          <h2 className='text-xl font-semibold'>₹193.20</h2>
+        </div>
+                <div className='flex w-full border-2 mb-2 active:border-black rounded-xl items-center p-3  justify-between'>
+       
+          <img className='h-14 mr-4' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+          <div className=' w-1/2'>
+            <h4 className='font-medium text-lg'>Moto <span><i className="ri-user-fill"></i>1</span></h4>
+            <h5 className='font-medium text-md'>3 mins away</h5>
+            <p className='font-medium text-sm text-gray-600'>Affordable, motorcycle rides</p>
+          </div>
+          <h2 className='text-xl font-semibold'>₹65.17</h2>
+        </div>
+              <div className='flex w-full border-2 mb-2 active:border-black rounded-xl items-center p-3  justify-between'>
+       
+          <img className='h-12 mr-4' src="https://www.svgrepo.com/show/395796/auto-rickshaw.svg" alt="" />
+          <div className=' w-1/2'>
+            <h4 className='font-medium text-lg'>Moto <span><i className="ri-user-fill"></i>3</span></h4>
+            <h5 className='font-medium text-md'>2 mins away</h5>
+            <p className='font-medium text-sm text-gray-600'>Affordable, Auto rides</p>
+          </div>
+          <h2 className='text-xl font-semibold'>₹118.21</h2>
+        </div>
+
+
+        
+
+       </div>
+
+       
+       
+   
     </div>
+
   )
 }
